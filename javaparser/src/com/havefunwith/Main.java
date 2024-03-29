d88882f348ef3e17c31d826df2b52cc13d54b620
Marlon Braga <41749546+mbraga24@users.noreply.github.com>
2024-03-28 20:50:20 -0500

    javaparser

---
+++ javaparser/src/com/havefunwith/Main.java
@@ -0,0 +1,21 @@
+package com.havefunwith;
+
+public class Main {
+    /**
+     * We want to implement a mini-language to control a simple turtle-graphics system.
+     * The language consists of single-letter commands, some followed by a single number.
+     * For example, the following input would draw a rectangle:
+     *
+     * P 2  # select pen 2
+     * D    # pen down
+     * W 2  # draw west 2cm
+     * N 1  # then north 1
+     * E 2  # then east 2
+     * S 1  # then back south
+     * U    # pen up
+     */
+    public static void main(String[] args) {
+	    System.out.println("parser");
+
+    }
+}
